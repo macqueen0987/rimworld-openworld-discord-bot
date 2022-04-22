45f73d3ac9e62b3219b8c895c37c50d751162911
macqueen0987 <44331678+macqueen0987@users.noreply.github.com>
2022-04-22 23:44:57 +0900

    Update rimworld.py

--- rimworld.py
+++ rimworld.py
@@ -178,7 +178,7 @@ def add_dlc(silent = True):
     dlc_link = var.dlc_link
     cwd = os.getcwd()
     os.chdir(var.server_dir + 'Whitelisted Mods/')
-    urlretrieve(var.dlc_link, "DLCs.zip")
+    urlretrieve(dlc_link, "DLCs.zip")
     with zipfile.ZipFile("DLCs.zip") as unzip:
         unzip.extractall("./")
     os.remove("DLCs.zip")
